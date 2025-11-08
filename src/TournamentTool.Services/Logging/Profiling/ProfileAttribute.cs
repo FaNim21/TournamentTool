3ce147a3aaae817e2e51a4a6cc30fcef1d2e088c
FaNim21 <filipekbwo@wp.pl>
2025-11-08 18:30:58 +0100

    profiling testing

---
+++ src/TournamentTool.Services/Logging/Profiling/ProfileAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using MethodBoundaryAspect.Fody.Attributes;
+
+namespace TournamentTool.Services.Logging.Profiling;
+
+[Serializable]
+[AttributeUsage(AttributeTargets.All)]
+public class ProfileAttribute : OnMethodBoundaryAspect
+{
+    [NonSerialized]
+    private Stopwatch? stopwatch;
+
+    public override void OnEntry(MethodExecutionArgs args)
+    {
+        if (!ProfilerManager.IsEnabled) return;
+
+        stopwatch = Stopwatch.StartNew();
+    }
+
+    public override void OnExit(MethodExecutionArgs args)
+    {
+        if (!ProfilerManager.IsEnabled || stopwatch == null) return;
+
+        stopwatch.Stop();
+        ProfilerManager.Report(args.Method, stopwatch.Elapsed);
+    }
+}
---
+++ src/TournamentTool.Services/Logging/Profiling/ProfilerManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+
+namespace TournamentTool.Domain.Attributes;
+
+
+public static class ProfilerManager
+{
+    public static bool IsEnabled { get; set; }
+    public static event Action<MethodBase, TimeSpan>? OnProfiled;
+
+    public static void Report(MethodBase method, TimeSpan time)
+    {
+        OnProfiled?.Invoke(method, time);
+    }
+}
